870caeebfbb39e43e4a0881e2019b7d10f1e2247
Eldar Akavov <desert73eagle@gmail.com>
2020-05-13 06:05:07 +0300

    adding /myAccount

--- src/main/java/com/onlineshop/diploma/DiplomaApplication.java
+++ src/main/java/com/onlineshop/diploma/DiplomaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DiplomaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DiplomaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DiplomaApplication.class, args);
+    }
 
 }
--- src/main/java/com/onlineshop/diploma/controller/HomeController.java
+++ src/main/java/com/onlineshop/diploma/controller/HomeController.java
@@ -9,4 +9,8 @@ public class HomeController {
     public String index(){
         return "index";
     }
+    @RequestMapping("/myAccount")
+    public String myAccount(){
+        return "myAccount";
+    }
 }
