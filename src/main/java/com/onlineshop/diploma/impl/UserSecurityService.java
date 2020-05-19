5bf33c9eea094a01c0124d4adbceeb3742938984
Eldar Akavov <desert73eagle@gmail.com>
2020-05-19 05:41:38 +0300

    adding UserSecurityService.java

---
+++ src/main/java/com/onlineshop/diploma/impl/UserSecurityService.java
@@ -0,0 +1,27 @@
+package com.onlineshop.diploma.impl;
+
+import com.onlineshop.diploma.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.onlineshop.diploma.domain.User;
+
+@Service
+public class UserSecurityService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+        User user = userRepository.findByUsername(username);
+
+        if (null == user){
+            throw new UsernameNotFoundException("Username not found");
+        }
+        return user;
+    }
+}
