558457cb1a270cc216f7aa326519002fe8977bd5
Eldar Akavov <desert73eagle@gmail.com>
2020-05-19 05:41:54 +0300

    adding UserRepository.java

---
+++ src/main/java/com/onlineshop/diploma/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.onlineshop.diploma.repository;
+
+import com.onlineshop.diploma.domain.User;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findByUsername(String username);
+}
