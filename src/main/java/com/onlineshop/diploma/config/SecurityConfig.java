a32035bd96019903c6ee994d5e0285ee6f82c3a0
Eldar Akavov <desert73eagle@gmail.com>
2020-06-02 05:58:22 +0300

    adding PUBLIC_MATCHERS

--- src/main/java/com/onlineshop/diploma/config/SecurityConfig.java
+++ src/main/java/com/onlineshop/diploma/config/SecurityConfig.java
@@ -32,7 +32,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/image/**",
             "/",
             "/newUser",
-            "/forgerPassword"
+            "/forgerPassword",
+            "/login",
+            "/fonts/**"
 
     };
 
