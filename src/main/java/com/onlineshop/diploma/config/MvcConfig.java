0e765d09608d0cbb680466e104a02a64dad5bad8
Eldar Akavov <desert73eagle@gmail.com>
2020-06-16 01:01:00 +0300

    fixing

---
+++ src/main/java/com/onlineshop/diploma/config/MvcConfig.java
@@ -0,0 +1,20 @@
+package com.onlineshop.diploma.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+    @Configuration
+    public class MvcConfig implements WebMvcConfigurer {
+
+        @Value("${upload.path}")
+        private String uploadPath;
+
+        @ Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            registry.addResourceHandler("/image/**")
+                    .addResourceLocations("file:///" + uploadPath + "/");
+        }
+    }
+
